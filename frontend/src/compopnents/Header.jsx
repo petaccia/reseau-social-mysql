f2706b81d01cf333b758827595549967f2dc2e13
PETACCIA <petaccia.seb@gmail.com>
2023-03-02 10:38:06 +0100

    mise en place des icons et des fonts pour le projet

--- frontend/src/compopnents/Header.jsx
+++ frontend/src/compopnents/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faCar } from "@fortawesome/free-solid-svg-icons"
 
 const Header = () => {
   return (
     <div className='header-container'>
       <h1>Je suis le header</h1>
+      <FontAwesomeIcon icon={faCar} />
     </div>
   )
 }
