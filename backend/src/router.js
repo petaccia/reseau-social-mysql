a32ffc3edac69b4bd68c545bd51d51ca2f9edbe8
PETACCIA <petaccia.seb@gmail.com>
2023-06-06 22:26:47 +0200

    mise en place des routes, controller, manager

--- backend/src/router.js
+++ backend/src/router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const connexionController = require("./controllers/ConnexionControllers");
 const userController = require("./controllers/UsersControllers");
-// const postController = require("./controllers/PostsControllers.js");
+const postController = require("./controllers/PostsControllers.js");
 const commentController = require("./controllers/CommentsControllers");
 // const likeController = require("./controllers/LikesControllers");
 const verificationInfo = require("./controllers/VerificationController");
@@ -19,11 +19,11 @@ router.put("/user/:id", userController.edit);
 router.delete("/user/:id", userController.destroy);
 
 // // routes posts
-// router.get("/post", postController.browse);
-// router.get("/find/:postId", postController.read);
-// router.post("/post", postController.add);
-// router.put("/post/:id", postController.edit);
-// router.delete("/post/:id", postController.destroy);
+router.get("/post", postController.getPostAll);
+router.get("/post/:id", postController.getPost);
+router.post("/post", postController.createPost);
+router.put("/post/:id", postController.updatePost);
+router.delete("/post/:id", postController.deletePost);
 
 // // routes comments
 router.get("/comment", commentController.getCommentAll);
