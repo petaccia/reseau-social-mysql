44cf8bc0caa6e6d81748c569042febebe69b0c9f
PETACCIA <petaccia.seb@gmail.com>
2023-07-20 22:44:23 +0200

    Création de la sidebar

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import Styles from "./Sidebar.module.scss";
+import logo from "../../assets/Logo/logo_noir.png";
+import { FcHome, FcPortraitMode, FcFeedback, FcMenu } from "react-icons/fc";
+import { NavLink, useLocation, Link } from "react-router-dom";
+
+const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const menuItem = [
+    {
+      path: "/",
+      name: "home",
+      icon: <FcHome />,
+    },
+    {
+      path: "/profile",
+      name: "Profile",
+      icon: <FcPortraitMode />,
+    },
+    {
+      path: "/contact",
+      name: "Contact",
+      icon: <FcFeedback />,
+    },
+  ];
+  const location = useLocation();
+
+  return (
+    <div className={Styles.container}>
+      <div
+        style={{ width: isOpen ? "13rem" : "4rem" }}
+        className={Styles.sidebar}
+      >
+        <div className={Styles.top_section}>
+          <div className={Styles.logoContainer}>
+            <Link to="/">
+            <img
+              src={logo}
+              style={{ display: isOpen ? "block" : "none" }}
+              className={Styles.logo}
+              alt="logo"
+            />
+            </Link>
+            <div
+              style={{ marginLeft: isOpen ? "8rem" : "0px" }}
+              className={Styles.bars}
+            >
+              <FcMenu onClick={toggle} />
+            </div>
+          </div>
+          <div className={Styles.iconContainer}>
+          {menuItem.map((item, index) => (
+            <NavLink
+              to={item.path}
+              key={index}
+              className={`${Styles.link} ${
+                location.pathname === item.path ? Styles.active : ""
+              }`}
+            >
+              <div className={Styles.icon}>{item.icon}</div>
+              <div
+                style={{ display: isOpen ? "block" : "none" }}
+                className={Styles.link_text}
+              >
+                {item.name}
+              </div>
+            </NavLink>
+          ))}
+          </div>
+        </div>
+      </div>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;
