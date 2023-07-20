1136170ff40db55b0ffe4e39de273fd5fb7ad4c9
PETACCIA <petaccia.seb@gmail.com>
2023-07-20 16:25:55 +0200

    Création des pages (home, profil, contact)

---
+++ frontend/src/pages/Contact/Contact.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Contact.module.scss";
+
+function Contact() {
+  return <div className={styles.contact}>Contact</div>;
+}
+
+export default Contact;
---
+++ frontend/src/pages/Profil/Profil.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Profil.module.scss";
+
+function Profil() {
+  return <div className={styles.profil}>Profil</div>;
+}
+
+export default Profil;
