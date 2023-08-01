1b98e0bd4feab92b9bf57c452e23c328d36e66ab
PETACCIA <petaccia.seb@gmail.com>
2023-08-01 09:55:31 +0200

    modification des imports

--- frontend/src/components/carousel/CarouselHomeStory/CarouselHomeStory.jsx
+++ frontend/src/components/carousel/CarouselHomeStory/CarouselHomeStory.jsx
@@ -1,56 +1,54 @@
-import React from 'react'
-import CardStory from '../../Cards/cardStory/CardStory';
+import React from "react";
+import CustomLeftArrowStory from "../../CustomCarousel/CustomCarouselHomeStory/CustomLeftArrowComment.jsx";
+import CustomRightArrowStory from "../../CustomCarousel/CustomCarouselHomeStory/CustomRightArrowComment .jsx";
+import CardStory from "../../Cards/cardStory/CardStory.jsx";
 
-import CustomCarousel from '../Carousel';
-import styles from './CarouselHomeStory.module.scss';
+import CustomCarousel from "../Carousel.jsx";
+import styles from "./CarouselHomeStory.module.scss";
 
+import chaval from "../../../assets/post/cheval.jpg";
+import requin from "../../../assets/post/requin.jpg";
+import maldive from "../../../assets/post/maldives.jpg";
+import noel from "../../../assets/post/noel.jpg";
 
-import chaval from '../../../assets/post/cheval.jpg';
-import requin from '../../../assets/post/requin.jpg';
-import maldive from '../../../assets/post/maldives.jpg';
-import noel from '../../../assets/post/noel.jpg';
-import CustomLeftArrowStory from '@components/CustomCarousel/CustomCarouselHomeStory/CustomLeftArrowComment';
-import CustomRightArrowStory from '@components/CustomCarousel/CustomCarouselHomeStory/CustomRightArrowComment c';
 const CarouselHomeStory = () => {
-
   const stories = [
     {
       image: chaval,
-      date: '12/12/2022',
-      author: 'Oceane',
+      date: "12/12/2022",
+      author: "Oceane",
     },
     {
       image: requin,
-      date: '12/12/2022',
-      author: 'Oceane',
+      date: "12/12/2022",
+      author: "Oceane",
     },
     {
       image: maldive,
-      date: '12/12/2022',
-      author: 'Oceane',
+      date: "12/12/2022",
+      author: "Oceane",
     },
     {
       image: noel,
-      date: '12/12/2022',
-      author: 'Oceane',
+      date: "12/12/2022",
+      author: "Oceane",
     },
     {
       image: chaval,
-      date: '12/12/2022',
-      author: 'Oceane',
+      date: "12/12/2022",
+      author: "Oceane",
     },
     {
       image: requin,
-      date: '12/12/2022',
-      author: 'Oceane',
+      date: "12/12/2022",
+      author: "Oceane",
     },
-   
   ];
   return (
     <div className={styles.home}>
       <CustomCarousel
-      customLeftArrow={<CustomLeftArrowStory />}
-      customRightArrow={<CustomRightArrowStory />}
+        customLeftArrow={<CustomLeftArrowStory />}
+        customRightArrow={<CustomRightArrowStory />}
       >
         {stories.map((story, index) => (
           <CardStory
@@ -62,7 +60,7 @@ const CarouselHomeStory = () => {
         ))}
       </CustomCarousel>
     </div>
-  )
-}
+  );
+};
 
-export default CarouselHomeStory
+export default CarouselHomeStory;
