4cca974c9056774668598c5b3332f4f99ba8d8f8
PETACCIA <petaccia.seb@gmail.com>
2023-08-02 16:41:51 +0200

    Modification du eslint et création du models Message

---
+++ backend/src/models/Messages.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+
+const db = require("../../databaseSequelize");
+
+const User = require("./User");
+
+const Message = db.define("messages", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  senderId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  receiverId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+});
+
+Message.belongsTo(User, { foreignKey: "senderId" });
+Message.belongsTo(User, { foreignKey: "receiverId" });
+
+module.exports = Message;
