b3fe5d85d0cbda8b3dbbf3a92522b3a9932abbcb
PETACCIA <petaccia.seb@gmail.com>
2023-08-04 16:26:58 +0200

    mise en place de la liste des messages

--- frontend/src/components/Lists/ListMessage/ListMessage.jsx
+++ frontend/src/components/Lists/ListMessage/ListMessage.jsx
@@ -1,45 +1,23 @@
 import React, { useContext, useEffect } from "react";
 import Styles from "./ListMessage.module.scss";
 import MessageContext from "../../../contexts/MessageContext/MessageContext.jsx";
+import CardMessage from "../../Cards/cardMessages/CardMessage.jsx";
 
 const MessageList = () => {
   const { messages, getMessages } = useContext(MessageContext);
 
   useEffect(() => {
-    getMessages(); 
-  });
+    getMessages();
+  }, []);
+  console.log(messages);
 
   return (
     <div className={Styles.containerList}>
-      <table className={Styles.table}>
-        <thead className={Styles.thead}>
-          <tr className={Styles.tr}>
-            <th>Identifiant</th>
-            <th>Titre</th>
-            <th>Description</th>
-            <th>Date</th>
-            <th>Expéditeur</th>
-            <th>Destinataire</th>
-            <th>Statut</th>
-          </tr>
-        </thead>
-        <tbody className={Styles.tbody}>
-          {messages.map((message) => (
-            <tr key={message.id}>
-              <td>{message.id}</td>
-              <td>{message.title}</td>
-              <td>{message.description}</td>
-              <td>{message.date}</td>
-              <td>{message.sender}</td>
-              <td>{message.receiver}</td>
-              <td>{message.status}</td>
-              <div className={Styles.buttonContainer}>
-                <button className={Styles.button}>Supprimer</button>
-              </div>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className={Styles.CardContainer}>
+        {messages.map((message) => (
+          <CardMessage key={message.id} message={message} />
+        ))}
+      </div>
     </div>
   );
 };
