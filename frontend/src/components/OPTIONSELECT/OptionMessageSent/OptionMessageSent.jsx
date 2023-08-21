ddc75adceebed324a7fcd0c73646e29d4fd83836
PETACCIA <petaccia.seb@gmail.com>
2023-08-21 16:29:48 +0200

    mise en place du select messages envoyés avec son fonctionnement

---
+++ frontend/src/components/OPTIONSELECT/OptionMessageSent/OptionMessageSent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OptionMessageSent = () => {
+  return (
+    <option value="sent">Messages envoyés</option>
+    )
+}
+
+export default OptionMessageSent
--- frontend/src/components/SELECTS/SelectMessage/SelectMessage.jsx
+++ frontend/src/components/SELECTS/SelectMessage/SelectMessage.jsx
@@ -1,50 +1,53 @@
-import React from 'react'
-import Styles from './SelectMessage.module.scss';
+import React from "react";
+import Styles from "./SelectMessage.module.scss";
 import OptionMessageDate from "../../OPTIONSELECT/OptionMessageDate/OptionMessageDate.jsx";
 import OptionMessageInProgress from "../../OPTIONSELECT/OptionMessageInProgress/OptionMessageInProgress.jsx";
 
-const SelectMessage = ({ onSort, messages}) => {
+const SelectMessage = ({ onSort, messages }) => {
   const sortByDate = (order) => {
     let sorted = [...messages];
     switch (order) {
-    case "desc_createdAt":
-      sorted.sort((a, b) => 
-         new Date(b.createdAt) - new Date(a.createdAt));
+      case "desc_createdAt":
+        sorted.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
         break;
-        case "asc_createdAt":
-      sorted.sort((a, b) => 
-        
-        new Date(a.createdAt) - new Date(b.createdAt));
+      case "asc_createdAt":
+        sorted.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
         break;
-        case "inProgress":
-      sorted = sorted.filter((messages) => {
-        return messages.statusRead === "inProgress";
-      })
-
-      };
-      onSort(sorted);
-      console.log("sorted",sorted)
+      case "inProgress":
+        sorted = sorted.filter((messages) => {
+          return messages.statusRead === "inProgress";
+        });
+        break;
+      case "sent":
+        sorted = sorted.filter((messages) => {
+          return messages.statusRead === "sent";
+        })
+        break;
     }
+    onSort(sorted);
+    console.log("sorted", sorted);
+  };
 
   return (
     <div className={Styles.selectMessage}>
-    <select 
-    name="message"
-    onChange={(e) => sortByDate (e.target.value)}
-    className={Styles.selectMessage}>
-      <option value="">Trier par</option>
-      <option value="all">Tous les messages</option>
-      <OptionMessageInProgress />
-      <option value="sent">Messages envoyés</option>
-      <option value="received">Messages reçus</option>
-      <option value="read">Messages lus</option>
-      <option value="unread">Messages non lus</option>
-     <OptionMessageDate  />
-      <option value="destination">Destinataire</option>
-      <option value="expéditeur">Expéditeur</option>
-    </select>
-  </div>
-  )
-}
+      <select
+        name="message"
+        onChange={(e) => sortByDate(e.target.value)}
+        className={Styles.selectMessage}
+      >
+        <option value="">Trier par</option>
+        <option value="all">Tous les messages</option>
+        <OptionMessageInProgress />
+        <option value="sent">Messages envoyés</option>
+        <option value="received">Messages reçus</option>
+        <option value="read">Messages lus</option>
+        <option value="unread">Messages non lus</option>
+        <OptionMessageDate />
+        <option value="destination">Destinataire</option>
+        <option value="expéditeur">Expéditeur</option>
+      </select>
+    </div>
+  );
+};
 
-export default SelectMessage
+export default SelectMessage;
