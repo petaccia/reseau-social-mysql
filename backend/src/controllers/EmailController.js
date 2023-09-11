e789fd5237d2b7de0db882e7dd4ba502381c3984
PETACCIA <petaccia.seb@gmail.com>
2023-09-11 15:16:56 +0200

    Création du controller validation et routes pour l'email et mise en fonction dans le frontend

---
+++ backend/src/controllers/EmailController.js
@@ -0,0 +1,69 @@
+const bcrypt = require("bcrypt");
+const Connection = require("../models/Connection");
+const { EmailValidation } = require("../services/validation/EmailValidation");
+
+// Vérifier l'email actuel si il existe
+const verifyEmail = async (req, res) => {
+  try {
+    const { userId, email, pass } = req.body;
+    const { error } = EmailValidation(req.body);
+    if (error) {
+      return res.status(400).json(error.details[0].message);
+    }
+    const connection = await Connection.findOne({
+      where: { id: userId },
+    });
+    if (!connection) {
+      return res.status(404).json("L'utilisateur n'existe pas");
+    }
+    // Comparer le mot de passe actuel
+    const validPassword = await bcrypt.compare(pass, connection.password);
+    if (!validPassword) {
+      return res.status(401).json("Mot de passe incorrect");
+    }
+    // Vérifier si l'email actuel correspond à l'email saisi par l'utilisateur
+    if (email !== connection.email) {
+      return res.status(401).json("Email incorrect");
+    }
+    return res.status(200).json("Email valide");
+  } catch (err) {
+    console.error("Erreur détaillée", err);
+    return res
+      .status(500)
+      .json({ message: "Une erreur est survenue", error: err });
+  }
+};
+
+// Mettre à jour l'email
+const updateEmail = async (req, res) => {
+  try {
+    const { userId, email } = req.body;
+
+    const { error } = EmailValidation(req.body);
+    if (error) {
+      return res.status(400).json(error.details[0].message);
+    }
+
+    const connection = await Connection.findOne({
+      where: { id: userId },
+    });
+
+    if (!connection) {
+      return res.status(404).json("L'utilisateur n'existe pas");
+    }
+
+    await Connection.update({ email }, { where: { id: userId } });
+
+    return res.status(200).json("Email mis à jour");
+  } catch (err) {
+    console.error("Erreur du serveur ", err.response.data);
+    return res
+      .status(500)
+      .json({ message: "Une erreur est survenue", error: err });
+  }
+};
+
+module.exports = {
+  verifyEmail,
+  updateEmail,
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -17,11 +17,11 @@ const eventController = require("./controllers/EventController");
 const statsController = require("./controllers/StatsController");
 const messageController = require("./controllers/MessageController");
 const passwordController = require("./controllers/PasswordController");
+const emailController = require("./controllers/EmailController");
 
 const LoginLimiter = require("./middleware/LoginLimiter");
 const upload = require("./services/multer");
 const setStatusReadBaseOrigin = require("./middleware/StatusRead");
-
 
 // Routes d'admin
 router.get("/admin", adminController.getAllAdmin);
@@ -53,6 +53,10 @@ router.put("/:id", connectionController.approveConnection);
 // Routes de password
 router.post("/verifyPassword", passwordController.verifyPassword);
 router.put("/user/password", passwordController.updatePassword);
+
+// Routes d' email
+router.post("/verifyEmail", emailController.verifyEmail);
+router.put("/user/email", emailController.updateEmail);
 
 // Routes de family
 router.get("/family", familyController.getAllFamily);
@@ -122,7 +126,11 @@ router.delete("/event/:id", eventController.deleteEvent);
 // Routes de message
 router.get("/message", messageController.getAllMessages);
 router.get("/message/:id", messageController.getMessage);
-router.post("/message", setStatusReadBaseOrigin, messageController.createMessage);
+router.post(
+  "/message",
+  setStatusReadBaseOrigin,
+  messageController.createMessage
+);
 router.put("/message/:id", messageController.updateMessage);
 router.delete("/message/:id", messageController.deleteMessage);
 router.delete("/messages", messageController.AllDeleteMessage);
