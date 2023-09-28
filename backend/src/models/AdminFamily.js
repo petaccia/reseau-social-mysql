90beae4cd8fc0fa4164fed363d18fa1493dda6fc
PETACCIA <petaccia.seb@gmail.com>
2023-09-28 14:10:32 +0200

    ajout de la fonction pour accepter ou refuser le user et modif de la table

--- backend/src/models/AdminFamily.js
+++ backend/src/models/AdminFamily.js
@@ -1,6 +1,8 @@
 const { DataTypes } = require("sequelize");
 const db = require("../../databaseSequelize");
 
+const Roles = require("./Roles");
+
 const AdminFamily = db.define("adminFamilies", {
   id: {
     type: DataTypes.INTEGER,
@@ -20,7 +22,16 @@ const AdminFamily = db.define("adminFamilies", {
   password: {
     type: DataTypes.STRING,
     allowNull: false,
-  }
+  },
+  roleId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Roles,
+      key: "id",
+    },
+  },
 });
 
+AdminFamily.belongsTo(Roles, { foreignKey: "roleId" });
 module.exports = AdminFamily;
