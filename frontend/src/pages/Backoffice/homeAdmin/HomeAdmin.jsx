c172ecbb1c02811a992bd47bc953d874c39ce875
PETACCIA <petaccia.seb@gmail.com>
2023-09-29 17:43:03 +0200

    Mise en place de fonction pour changer le status des user par l'adminfamily avec les bouttons

--- frontend/src/pages/Backoffice/homeAdmin/HomeAdmin.jsx
+++ frontend/src/pages/Backoffice/homeAdmin/HomeAdmin.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import apiConnect from "../../../services/API/apiConnection.jsx";
 import styles from "./HomeAdmin.module.scss";
+import {
+  toastSuccess,
+  toastError,
+} from "../../../services/Toastify/toastConfig.jsx";
 
 const HomeAdmin = () => {
   const [familyMembers, setFamilyMembers] = useState([]);
@@ -16,10 +20,44 @@ const HomeAdmin = () => {
       }
     };
 
-
-
     getFamilyMembers();
+    changeStatus();
   }, []);
+
+  // Changement de couleur en fonction du statut
+  const changeColor = (status) => {
+    switch (status) {
+      case "pending":
+        return styles.pending;
+      case "accepted":
+        return styles.accepted;
+      case "refused":
+        return styles.refused;
+      default:
+        return styles.pending;
+    }
+  };
+
+  // Changement du statut de la l'inscription d'un membre de la famille pour validation ou refuse
+  const changeStatus = async (userId, status) => {
+    try {
+      const response = await apiConnect.put("/adminfamily/user/accept", {
+        userId,
+        status,
+      });
+      console.log(response.data);
+      if (response.status === 200) {
+        toastSuccess("La demande a été traitée avec succès");
+      } else {
+        toastError("Erreur lors de la validation de la demande");
+      }
+      return response;
+    } catch (error) {
+      console.error(error);
+      toastError("Une erreur est survenue");
+      return error;
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -40,7 +78,7 @@ const HomeAdmin = () => {
         </thead>
         <tbody>
           {familyMembers.map((members) => (
-            <tr className={styles.tabMembers}>
+            <tr className={styles.tabMembers} key={members.id}>
               <td className={styles.name}>{members.name}</td>
               <td className={styles.firstname}>{members.firstname}</td>
               <td className={styles.phone}>{members.phone}</td>
@@ -49,6 +87,20 @@ const HomeAdmin = () => {
               <td className={styles.postalCode}>{members.postalCode}</td>
               <td className={styles.country}>{members.country}</td>
               <td className={styles.status}>{members.status}</td>
+              <td className={`${styles.status} ${changeColor(members.status)}`}>
+                <button
+                  onClick={() => changeStatus(members.id, "accepté")}
+                  className={styles.button}
+                >
+                  Accepter
+                </button>
+                <button
+                  onClick={() => changeStatus(members.id, "refusé")}
+                  className={styles.button}
+                >
+                  Refuser
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
