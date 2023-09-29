addeaa6b10b20ea7cb71feb75307a8ff2f5e7266
PETACCIA <petaccia.seb@gmail.com>
2023-09-29 15:04:58 +0200

    Modification pour mettre le token dans le cookie et intégration du middleware authjwt pour authentification et requireRole pour la protection des routes par roles

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -4,53 +4,71 @@ const User = require("../models/User");
 
 // Middleware authentication
 const authenticateJWT = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).json({ message: "Entête d'autorisation manquante" });
-  }
-  const token = authHeader.split(" ")[1];
+  const { token } = req.cookies;
+  console.log("token in authenticateJWT back", token);
   if (!token) {
-    return res.status(401).json({ message: "il manque le token" });
+    return res.status(401).json({ message: "Pas de token" });
   }
+
   try {
-    const decodedToken = jwt.verify(token, process.env.DB_ACCESS_TOKEN_SECRET);
-    req.user = decodedToken;
+    // const secret = "3$dt&NE95bgtRRjhLs5yAMf54sP$c8$x8!@";
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded.userType === "adminFamily") {
+      req.user = await AdminFamily.findOne({ where: { id: decoded.id } });
+    } else if (decoded.userType === "user") {
+      req.user = await User.findOne({ where: { id: decoded.id } });
+    }
+
+    if (!req.user) {
+      return res.status(401).json({ message: "Token invalide" });
+    }
     next();
   } catch (err) {
     console.error(err);
-    return res.status(401).json({ message: "Token non valide ou expiré" });
+    return res.status(500).json({ message: "Erreur de token", error: err });
   }
 };
-
-// middleware pour le role requis pour la permission
+//  middleware pour le role requis pour la permission
 const requireRole = (role) => {
+  
   return (req, res, next) => {
-    if (req.user && req.user.role === role) {
-      next();
+    console.log("Inside requireRole middleware", req.user);
+    console.log("Expected role:", role);
+    console.log("Actual user role:", req.user ? req.user.roleId : "No user");
+        if (req.user && (role === "adminfamily" ? req.user.roleId === 1 : true)) {
+          console.log("Role check passed. Moving to next middleware.");
+      return next();
     } else {
-      res.status(403).json({ message: "Accès refusé" });
-    }
+      console.log("Role check failed. Sending 403 response.");
+      return res.status(403).json({ message: "Accès refusé" });
+    } 
   };
 };
 
-const checkUserStatus = (req, res, next) => {
-  const model = req.user.role === "adminFamily" ? AdminFamily : User;
-  model
-    .findOne({ where: { id: req.user.id } })
-    .then((user) => {
-      if (!user) {
-        res.status(403).json({ message: "Accès refusé" });
-      } else if (user.status !== "accepté") {
-        res
-          .status(401)
-          .json({ message: "Votre compte est en attente de validation" });
-      }
-      next();
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).json({ message: "Erreur du serveur" });
-    });
-};
+// const checkUserStatus = (req, res, next) => {
+//   console.log("Inside checkUserStatus middleware");  
+//   const model = req.user.userType === "adminFamily" ? AdminFamily : User;
+//   model
+//     .findOne({ where: { id: req.user.id } })
+//     .then((user) => {
+//       if (!user) {
+//         return res.status(404).json({ message: "Utilisateur introuvable" });
+//       }
+//       if (user.userType !== "accepté") {
+//         return res
+//           .status(401)
+//           .json({ message: "Votre compte est en attente de validation" });
+//         }
+//         return next();
+//     })
+//     .catch((err) => {
+//       return res.status(500).json({ message: "Erreur du serveur" });
+//     });
+// };
 
-module.exports = { authenticateJWT, requireRole, checkUserStatus };
+module.exports = 
+{ 
+  authenticateJWT,
+  requireRole,
+  //  checkUserStatus 
+  };
