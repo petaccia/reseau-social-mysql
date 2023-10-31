96534169e655a762a1fab8df78d87b740b7e9a95
PETACCIA <petaccia.seb@gmail.com>
2023-10-31 13:59:54 +0100

    création d'une condition pour que la page home affiche soit la page mobile ou la page Desktop

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import HomeDesktop from "../../components/Desktop/HomeDesktop/HomeDesktop.jsx";
+import HomeMobile from "../../components/Mobile/HomeMobile/HomeMobile.jsx";
 
 const Home = () => {
-  return (
-    <div>
-      <HomeDesktop />
-    </div>
-  );
+  // Créer une condition si la page est à afficher en mobile ou desktop
+  const isMobile = window.innerWidth < 768;
+  return <div>{isMobile ? <HomeMobile /> : <HomeDesktop />}</div>;
 };
 
 export default Home;
