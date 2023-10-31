70f3f81a423bb045f5f2a634178fc69dd7645e59
PETACCIA <petaccia.seb@gmail.com>
2023-10-31 11:38:37 +0100

    Création du composant logo dans accessories pour pouvoir l'utiliser plus facilement et un code plus propre

---
+++ frontend/src/components/Accessories/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../../assets/Logo/logo_noir.png";
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={logo} alt=" symbol du logo" />
+    </div>
+  );
+};
+
+export default Logo;
