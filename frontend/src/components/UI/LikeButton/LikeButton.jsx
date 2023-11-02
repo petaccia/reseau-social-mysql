8e47d6caf01f7f2c412bffb40573b9395ec71a37
PETACCIA <petaccia.seb@gmail.com>
2023-11-02 16:35:26 +0100

    déplacement du fichier dans le dossier UI et modification du fichier pour le compteur de likes

---
+++ frontend/src/components/UI/LikeButton/LikeButton.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import  "./LikeButton.scss";
+import { FaRegHeart } from "react-icons/fa";
+
+const LikeButton = () => {
+  const [likeCount, setLikeCount] = useState(0);
+  const [liked, setLiked] = useState(false);
+
+  const handleLike = () => {
+    setLiked(true);
+    setLikeCount(likeCount === 0 ? 1 : likeCount + 1);
+
+  };
+  return (
+    <div className="containerHeart" onClick={handleLike}>
+      <span className="heartText">Likes</span>
+      <FaRegHeart size={20} color={liked ? "#ff2afb" : " #3d4a5e"} />
+        <span className="likes">{likeCount}</span>
+    </div>
+  );
+};
+
+export default LikeButton;
