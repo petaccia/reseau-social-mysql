2cf3e1f9902c1c7d525a10550340391b094f8ea8
PETACCIA <petaccia.seb@gmail.com>
2023-11-02 14:49:56 +0100

    Mise en place de la listUser qui sera connecté

--- frontend/src/components/Lists/listUser/ListUser.jsx
+++ frontend/src/components/Lists/listUser/ListUser.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 import CardUser from "../../Cards/cardUser/CardUser.jsx";
+import "./ListUser.scss";
 
 const ListUser = ({ users }) => {
   return (
-    <div>
+    <div className=" listUserContainer d-flex flex-sm-row flex-md-column overflow-auto py-2 px-1 w-100">
       {users.map((user) => (
-        <CardUser key={user.id} user={user} />
+        <React.Fragment key={user.id}>
+          <div
+            className="cardMobileListUser  d-flex align-items-center ms-5 mr-3 p-1 bg-subtle border rounded-3 d-sm-flex d-md-none"
+            style={{ boxShadow: "0px 10px 20px 0px #2aa6ff" }}
+          >
+            <div
+              className="user-status mr-2 "
+              style={{
+                backgroundColor: user.isConnected ? "#28a745" : "#dc3545",
+                width: "8px",
+                height: "8px",
+                borderRadius: "50%",
+              }}
+            ></div>
+            <img
+              src={user.image}
+              className="rounded-circle ms-2"
+              style={{
+                width: "40px",
+                height: "40px",
+                objectFit: "cover",
+                marginRight: "10px",
+              }}
+              alt="avatar"
+            />
+            <h2
+              className="name ml-1 mb-0"
+              style={{ fontSize: "14px", color: "#2aa6ff" }}
+            >
+              {user.name}
+            </h2>
+          </div>
+          <div className="d-none d-md-flex">
+            <CardUser user={user} />
+          </div>
+        </React.Fragment>
       ))}
     </div>
   );
