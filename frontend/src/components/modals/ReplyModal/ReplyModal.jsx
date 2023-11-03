529d24379cd8bf2ea29c733789060e883ea4a69a
PETACCIA <petaccia.seb@gmail.com>
2023-11-03 15:12:29 +0100

    Création de la modale de réponse au commentaire avec son style et mise en place dans la cardPost

--- frontend/src/components/modals/ReplyModal/ReplyModal.jsx
+++ frontend/src/components/modals/ReplyModal/ReplyModal.jsx
@@ -1,82 +1,48 @@
-import React from "react";
-import Styles from "./ReplyModal.module.scss";
-import { toastSuccess, toastError, toastWarning } from "../../../services/Toastify/toastConfig.jsx";
+import React, { useState } from "react";
+import { Modal, Form, Button, CloseButton } from "react-bootstrap";
+import "./ReplyModal.scss";
 
-const ReplyModal = ( {isReplyModalOpen, closeReplyModal, replyTextHandler, title, description, message, check, sender, receiver, replyText, sendMessage }) => {
-  
+const ReplyModal = ({ handleClose, commentId }) => {
+  // Etat pour le contenu du commentaire
+  const [reply, setReply] = useState("");
 
-    // Envoie de la réponse
-    const sendReply = async () => {
-      // Vérification si le champ de réponse est vide
-      if (!replyText) {
-        toastWarning("Veuillez remplir le champ de réponse");
-        return;
-      }
-      try {
-        await sendMessage({
-          title: message.title,
-          description: message.description,
-          message: replyText,
-          senderId: sender,
-          receiverId: receiver,
-          status: check,
-        });
-        toastSuccess("Votre message a bien été envoyé");
-        closeReplyModal();
-      } catch (error) {
-        console.error(error);
-        toastError("Erreur lors de l'envoi du message", {
-          className: Styles.toastSuccess,
-          style: {
-            top: "150px",
-            right: "100px",
-            boxShadow: "5px 2px 1px red",
-            backgroundColor: "#10131e",
-            color: "red",
-          },
-        });
-      }
-    };
+  // Fonction pour soumettre la reponse au commentaire
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(` Reply to comment ${commentId}: ${reply}`);
+    setReply("");
+    handleClose();
+  };
 
-  
-
-  
   return (
-    <div className={Styles.containerReplyModal}>
-    {isReplyModalOpen && (
-      <div className={Styles.modal}>
-        <div className={Styles.modalHeader}>
-          <h6 className={Styles.modalTitle}>Titre </h6>
-          <p className={Styles.modalMessageTitle}> {title}</p>
-        </div>
-        <button className={Styles.buttonClose} onClick={closeReplyModal}>
-          fermer
-        </button>
-        <div className={Styles.containerModalDescription}>
-          <h6 className={Styles.modalDescription}>description :</h6>
-          <p className={Styles.description}>{description}</p>
-        </div>
-        <div className={Styles.containerModalContent}>
-          <textarea
-            className={Styles.textarea}
-            type="text"
-            id=""
-            cols="90"
-            rows="5"
-            value={replyText}
-            onChange={replyTextHandler}
-            placeholder="Votre réponse ..."
-          />
-          <div className={Styles.containerModalButton}>
-            <button className={Styles.buttonSend} onClick={sendReply}>
-              Envoyer
-            </button>
-          </div>
-        </div>
-      </div>
-    )}
-  </div>
-)
+    <Modal show={true} onHide={handleClose} className="modal-reply-comment  ">
+      <Modal.Header className="modal-reply-comment-header">
+        <Modal.Title className="modal-reply-comment-title">
+          Répondre au commentaire
+        </Modal.Title>
+        <CloseButton variant="white" onClick={handleClose} />
+      </Modal.Header>
+      <Modal.Body className="modal-reply-comment-body">
+        <Form onSubmit={handleSubmit} className="form-reply-comment">
+          <Form.Group className="form-group-reply-comment mb-3">
+            <Form.Label className="form-label-reply-comment text-primary">
+              Contenu du commentaire
+            </Form.Label>
+            <Form.Control
+              className="form-control-reply-comment"
+              as="textarea"
+              rows={3}
+              value={reply}
+              onChange={(e) => setReply(e.target.value)}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Soumettre
+          </Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
 };
 
-export default ReplyModal
+export default ReplyModal;
