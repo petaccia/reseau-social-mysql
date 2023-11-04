d9353792a9294fe04874123bb18d4c6eb04fea66
PETACCIA <petaccia.seb@gmail.com>
2023-11-04 19:42:52 +0100

    Création de la page profil mobile et déplacement de la page profil dans le composant pageProfildesktop

---
+++ frontend/src/components/Mobile/PageProfilMobile/PageProfilMobile.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import CarouselHomeStory from "../../carousel/CarouselHomeStory/CarouselHomeStory.jsx";
+import ListUser from "../../Lists/listUser/ListUser.jsx";
+import oceane from "../../../assets/users/oceane.jpg";
+import imgProfil from "../../../assets/illustration/profil_test.jpg";
+
+import cheval from "../../../assets/post/cheval.jpg";
+import requin from "../../../assets/post/requin.jpg";
+import maldive from "../../../assets/post/maldives.jpg";
+import noel from "../../../assets/post/noel.jpg";
+import CardPost from "../../Cards/cardPost/CardPost.jsx";
+
+const PageProfilMobile = () => {
+
+  const posts = [
+    {
+      id: 1,
+      title: "balade en cheval",
+      content: "la balade en cheval au pays des chevaux",
+      image: cheval,
+      user: "Oceane",
+      date: "12/12/2022",
+    },
+    {
+      id: 2,
+      title: "balade en mer",
+      content: "la balade en mer au pays des requins",
+      image: requin,
+      user: "Oceane",
+      date: "12/12/2022",
+    },
+    {
+      id: 3,
+      title: "Les vacances aux Maldives",
+      content: "Super vacances aux Maldives",
+      image: maldive,
+      user: "Oceane",
+      date: "12/12/2022",
+    },
+    {
+      id: 4,
+      title: "Joyeux Noël à tous!",
+      content: "C'est bon c'est parfait le Noël en famille",
+      image: noel,
+      user: "Oceane",
+      date: "12/12/2022",
+    },
+  ];
+
+
+  const users = [
+    {
+      id: 1,
+      name: "Oceane",
+      image: oceane,
+    },
+    {
+      id: 2,
+      name: "Oceane",
+      image: oceane,
+    },
+    {
+      id: 3,
+      name: "Oceane",
+      image: oceane,
+    },
+    {
+      id: 4,
+      name: "Oceane",
+      image: oceane,
+    },
+    {
+      id: 5,
+      name: "Oceane",
+      image: oceane,
+    },
+    {
+      id: 6,
+      name: "Oceane",
+      image: oceane,
+    },
+    {
+      id: 7,
+      name: "Oceane",
+      image: oceane,
+    },
+    {
+      id: 8,
+      name: "Oceane",
+      image: oceane,
+    },
+    {
+      id: 9,
+      name: "Oceane",
+      image: oceane,
+    },
+    {
+      id: 10,
+      name: "Oceane",
+      image: oceane,
+    },
+    {
+      id: 11,
+      name: "Oceane",
+      image: oceane,
+    },
+  ];
+
+  // Etat pour savoir si l'utilisateur est connecté ou non
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  return (
+    <div className="page-profil-mobile d-flex w-100 justify-content-center flex-column align-items-center ">
+        <div className="w-100 d-flex justify-content-center">
+        <img className=" profil w-100" src={imgProfil} alt="profil" />
+      </div>
+      <CarouselHomeStory />
+      {isLoggedIn && <ListUser users={users} />}
+      <div className="card-post-container-mobile w-100 d-flex flex-column align-items-center justify-content-center">
+        {posts.map((post) => (
+          <CardPost key={post.id} post={post} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PageProfilMobile;
--- frontend/src/pages/Profil/Profil.jsx
+++ frontend/src/pages/Profil/Profil.jsx
@@ -1,101 +1,10 @@
 import React from "react";
-import ListUser from "../../components/Lists/listUser/ListUser.jsx";
-import styles from "./Profil.module.scss";
-import CardPost from "../../components/Cards/cardPost/CardPost.jsx";
-
-import cheval from "../../assets/post/cheval.jpg";
-import requin from "../../assets/post/requin.jpg";
-import maldive from "../../assets/post/maldives.jpg";
-import noel from "../../assets/post/noel.jpg";
-import profil from "../../assets/profil/profil_test.jpg";
+import { useMediaQuery } from "react-responsive";
+import PageProfilDesktop from "../../components/Desktop/PageProfilDesktop/PageProfilDesktop.jsx";
+import PageProfilMobile from "../../components/Mobile/PageProfilMobile/PageProfilMobile.jsx";
 
 const Profil = () => {
-  const posts = [
-    { id: 1, title: "balade en cheval", content: "Content 1", image: cheval },
-    { id: 2, title: "Post 2", content: "Content 2", image: requin },
-    { id: 3, title: "Post 3", content: "Content 3", image: maldive },
-    { id: 4, title: "Post 4", content: "Content 4", image: noel },
-  ];
-
-  const users = [
-    {
-      id: 1,
-      name: "Oceane",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 2,
-      name: "Oceane",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 3,
-      name: "Oceane",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 4,
-      name: "Oceane",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 5,
-      name: "Oceane",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 6,
-      name: "Oceane",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 7,
-      name: "Oceane",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 8,
-      name: "Oceane",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 9,
-      name: "Oceane",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 10,
-      name: "Oceane",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 11,
-      name: "Oceane",
-      image: "https://picsum.photos/200/300",
-    },
-  ];
-
-  return (
-    <div className={styles.containerPage}>
-      <div className={styles.posts}>
-        <div className={styles.containerImg}>
-          <img src={profil} alt="avatar" className={styles.img} />
-          <h1 className={styles.name}>Oceane</h1>
-        </div>
-        {posts.map((post) => (
-          <CardPost
-            key={post.id}
-            title={post.title}
-            content={post.content}
-            image={post.image}
-          />
-        ))}
-      </div>
-      <div className={styles.userList}>
-        <ListUser users={users} />
-      </div>
-    </div>
-  );
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  return isMobile ? <PageProfilMobile /> : <PageProfilDesktop />;
 };
-
 export default Profil;
