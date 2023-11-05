896ff30711ffb52a5a6333e85045dba16afa9fc9
PETACCIA <petaccia.seb@gmail.com>
2023-11-05 22:13:14 +0100

    Mise enplace du formulaire de contact pour par version mobile et responsive

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -1,12 +1,11 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 import PageContactDesktop from "../../components/Desktop/PageContactDesktop/PageContactDesktop.jsx";
+import PageContactMobile from "../../components/Mobile/PageContactMobile/PageContactMobile.jsx";
 
 const Contact = () => {
-  return (
-    <div>
-      <PageContactDesktop />
-    </div>
-  );
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  return isMobile ? <PageContactMobile /> : <PageContactDesktop />;
 };
 
 export default Contact;
